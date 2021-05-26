e1bbc4b3479e9ba34c42bb056c011a5f6f2a3603
lamisghanayem <ghanayemlamis@gmail.com>
2021-05-26 22:40:41 +0300

    update from lamis

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-'use strict';
+/*'use strict';
 
 // it is just a comment
 
@@ -141,6 +141,110 @@ fivQues();
 numGussGame();
 
 guessColor();
+
+alert(`your score is ${score}`);*/
+
+
+
+'use strict';
+function FirstGame (){
+  alert('Answer the next 5 questions with yes/no or y/n ...');
+  let ownerLocation = prompt('Do i live in Amman ?');
+  ownerLocation = ownerLocation.toLowerCase();
+  if( ownerLocation === 'yes' || ownerLocation === 'y') {
+  // console.log("No I live in Ma'an");
+    alert('No I live in Ma\'an');
+  }else{
+  // console.log("correct I live in Ma'an");
+    alert('correct I live in Ma\'an');
+  }
+  let ownerHobby = prompt('Do I like video games ?');
+  ownerHobby = ownerHobby.toLowerCase();
+  if( ownerHobby === 'yes' || ownerHobby === 'y') {
+  // console.log('You bet, I love video games');
+    alert('You bet, I love video games');
+  }else{
+  // console.log('Really, I thought you know me more 😭');
+    alert('Really, I thought you know me more 😭');
+  }
+  let ownerPet = prompt('Do I have any pets ?');
+  ownerPet = ownerPet.toLowerCase();
+  if( ownerPet === 'yes' || ownerPet === 'y') {
+  // console.log('Correct, I have a cat names Blacky, I love this guy');
+    alert('Correct, I have a cat names Blacky, I love this guy');
+  }else{
+  // console.log('Wrong answer, my cat Blacky would be mad at you &#58587 ');
+    alert('Wrong answer, my cat Blacky would be mad at you 😺 ');
+  }
+  let ownerCar = prompt('Do i have a car ?');
+  ownerCar = ownerCar.toLowerCase();
+  if( ownerCar === 'yes' || ownerCar === 'y') {
+  // console.log('Correct, I have a Mitsubishi Galant 2.0 2006 car');
+    alert('Correct, I have a Mitsubishi Galant 2.0 2006 car');
+  }else{
+  // console.log('Wrong answer, I have a Mitsubishi Galant 2.0 2006 car, VROOM VROOM my friend :D ');
+    alert('Wrong answer, I have a Mitsubishi Galant 2.0 2006 car, VROOM VROOM my friend :D ');
+  }
+  let ownerEyes = prompt('Do I wear glasses ?');
+  ownerEyes = ownerEyes.toLowerCase();
+  if( ownerEyes === 'yes' || ownerEyes === 'y') {
+  // console.log('Correct, I wear glasses because im short sight');
+    alert('Correct, I wear glasses because im short sighted');
+  }else{
+  // console.log('Wrong answer, I wear glasses because im short sighted');
+    alert('Wrong answer, I wear glasses because im short sighted');
+  }
+  let userName = localStorage.getItem('username');
+  document.getElementById('h1').innerHTML = `You have finished the quiz ${userName}`;
+}
+function SecondGame (){
+  alert('Now we will play a guessing game');
+  const rightNum = 7 ;
+  let score = 0;
+  for(let i = 1; i<= 4 ; i++){
+    let guessNum = prompt('Try to guess the number, HINT: the number is between 0-10');
+    guessNum = Number(guessNum);
+    if(guessNum === rightNum){
+      alert('You guessed right');
+      score++;
+      break;
+    }else if (guessNum < rightNum){
+      alert('You are lower than the number');
+    }else if (guessNum > rightNum){
+      alert('you are higher than the number');
+    }
+    if(i === 4){
+      alert(`You exhausted your attempts, the right answer is ${rightNum}`);
+    }
+  }
+}
+
+function ThirdGame(){
+  const colorArr = ['black','red','white'];
+  guessGame:
+  for(let i = 1; i<= 6 ; i++){
+    let guessColor = prompt('Try to guess my favorite color ?').toLowerCase();
+    for(let index = 0 ; index < colorArr.length; index++ ){
+      if(guessColor === colorArr[index]){
+        alert('You guessed right');
+        score++;
+        break guessGame;
+      }
+    }
+  }
+
+  let stringArr = ' ';
+  for(let index2 = 0 ; index2 < colorArr.length; index2++){
+    stringArr = stringArr + colorArr[index2] + ', ' ;
+  }
+
+  alert(`The correct answers are ${stringArr}`);
+  alert(`The correct answers are ${colorArr}`);
+}
+
+FirstGame();
+SecondGame();
+ThirdGame();
 
 alert(`your score is ${score}`);
 
