3ca8d286f770b95df24b6f7a8ff4df9e41a8c9e0
OsaidAlhomedy <o.alhomedy@gmail.com>
2021-05-27 17:05:33 +0300

    changes to the page styling

--- README.md
+++ README.md
@@ -8,6 +8,8 @@ This is a guessing game website about myself, lets see who knows me :)
 * The Navigator is : Aseed Alhameedi
 
 ### Author : Osaid Alhomedy
+
 Links and Resources
+
 * [W3Schoo Css](https://www.w3schools.com/css/default.asp)
 * [Stackoverflow](https://stackoverflow.com/questions/27355397/how-to-get-variable-value-from-another-js-file)
--- js/app.js
+++ js/app.js
@@ -1,6 +1,5 @@
 'use strict';
 
-// it is just a comment
 
 let score=0;
 
@@ -11,12 +10,10 @@ function fivQues(){
   ownerLocation = ownerLocation.toLowerCase();
   if( ownerLocation === 'yes' || ownerLocation === 'y') {
 
-    // console.log("No I live in Ma'an");
     alert("No I live in Ma'an");
 
   }else{
 
-    // console.log("correct I live in Ma'an");
     alert("correct I live in Ma'an");
     score++;
 
@@ -27,13 +24,11 @@ function fivQues(){
   ownerHobby = ownerHobby.toLowerCase();
   if( ownerHobby === 'yes' || ownerHobby === 'y') {
 
-    // console.log('You bet, I love video games');
     alert('You bet, I love video games');
     score++;
 
   }else{
 
-    // console.log('Really, I thought you know me more 😭');
     alert('Really, I thought you know me more 😭');
   }
 
@@ -41,13 +36,11 @@ function fivQues(){
   ownerPet = ownerPet.toLowerCase();
   if( ownerPet === 'yes' || ownerPet === 'y') {
 
-    // console.log('Correct, I have a cat names Blacky, I love this guy');
     alert('Correct, I have a cat names Blacky, I love this guy');
     score++;
 
   }else{
 
-    // console.log('Wrong answer, my cat Blacky would be mad at you &#58587 ');
     alert('Wrong answer, my cat Blacky would be mad at you 😺 ');
 
   }
@@ -56,13 +49,11 @@ function fivQues(){
   ownerCar = ownerCar.toLowerCase();
   if( ownerCar === 'yes' || ownerCar === 'y') {
 
-    // console.log('Correct, I have a Mitsubishi Galant 2.0 2006 car');
     alert('Correct, I have a Mitsubishi Galant 2.0 2006 car');
     score++;
 
   }else{
 
-    // console.log('Wrong answer, I have a Mitsubishi Galant 2.0 2006 car, VROOM VROOM my friend :D ');
     alert('Wrong answer, I have a Mitsubishi Galant 2.0 2006 car, VROOM VROOM my friend :D ');
 
   }
@@ -71,13 +62,11 @@ function fivQues(){
   ownerEyes = ownerEyes.toLowerCase();
   if( ownerEyes === 'yes' || ownerEyes === 'y') {
 
-    // console.log('Correct, I wear glasses because im short sight');
     alert('Correct, I wear glasses because im short sighted');
     score++;
 
   }else{
 
-    // console.log('Wrong answer, I wear glasses because im short sighted');
     alert('Wrong answer, I wear glasses because im short sighted');
 
   }
